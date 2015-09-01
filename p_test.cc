cd80884d78aca1517899af1b9ecbe59e40ccaa5d
James Gallagher <jgallagher@opendap.org>
2015-09-01 14:15:42 -0600

    Added options for time, messages and output

--- p_test.cc
+++ p_test.cc
@@ -24,10 +24,6 @@
 using namespace std;
 using namespace libdap;
 
-#undef TIME 
-#define MSG(x)
-//#define MSG(x) do { x; } while(false)
-
 double time_diff_to_hundredths(struct timeval *stop, struct timeval *start)
 {
   /* Perform the carry for the later subtraction by updating y. */
@@ -54,15 +50,27 @@ main(int argc, char *argv[])
 {
     int repetitions = 1;
     bool threads = false;
+    bool time = false;
+    bool messages = false;
+    string outfile = "";
     char ch;
 
-    while ((ch = getopt(argc, argv, "tr:")) != -1) {
+    while ((ch = getopt(argc, argv, "tr:Tmo:")) != -1) {
       switch (ch) {
       case 't':
 	threads = true;
 	break;
+      case 'T':
+	time = true;
+	break;
+      case 'm':
+	messages = true;
+	break;
       case 'r':
 	repetitions = atoi(optarg);
+	break;
+      case 'o':
+	outfile = optarg;
 	break;
       case '?':
       default:
@@ -100,28 +108,31 @@ main(int argc, char *argv[])
     MarshallerThread *mt = new MarshallerThread();
 
     // To simulate the open stream/network, open once.
-    string outfile = infile;
-    outfile.append(".out");
+    if (outfile.empty()) {
+        string outfile = infile;
+	outfile.append(".out");
+    }
     ofstream out(outfile.c_str(), ios::out);
 
     for (int i = 0; i < repetitions; ++i) {
         {
             // to simulate our problem, open and read for each iteration
-  	    MSG(cerr << "open and read..." << endl);
-#if TIME
+  	    if (messages) cerr << "open and read..." << endl;
+
 	    struct timeval tp_s;
-	    if (gettimeofday(&tp_s, 0) != 0)
-	      cerr << "could not read time" << endl;
-#endif
-            ifstream in(infile.c_str(), ios::in);
-            in.read(&buf[0], bytes);
-#if TIME
-	    struct timeval tp_e;
-	    if (gettimeofday(&tp_e, 0) != 0)
+	    if (time && gettimeofday(&tp_s, 0) != 0)
 	      cerr << "could not read time" << endl;
 
-	    cerr << "time to read: " << time_diff_to_hundredths(&tp_e, &tp_s) << endl;
-#endif
+            ifstream in(infile.c_str(), ios::in);
+            in.read(&buf[0], bytes);
+
+	    struct timeval tp_e;
+	    if (time) {
+	      if (gettimeofday(&tp_e, 0) != 0)
+		cerr << "could not read time" << endl;
+	      
+	      cerr << "time to read: " << time_diff_to_hundredths(&tp_e, &tp_s) << endl;
+	    }
         }
 
         {
@@ -129,40 +140,43 @@ main(int argc, char *argv[])
             memcpy(tmp, &buf[0], bytes);
 
             if (threads) {
-	        MSG(cerr << "writing using a child thread..." << endl);
-#if TIME
+	      if (messages) cerr << "writing using a child thread..." << endl;
+
 		struct timeval tp_s;
-		if (gettimeofday(&tp_s, 0) != 0)
-		    cerr << "could not read time" << endl;
-#endif
+		if (time && gettimeofday(&tp_s, 0) != 0)
+		  cerr << "could not read time" << endl;
+
                 Locker lock(mt->get_mutex(), mt->get_cond(), mt->get_child_thread_count());
                 mt->increment_child_thread_count();
                 // thread deletes tmp
                 mt->start_thread(libdap::MarshallerThread::write_thread, out, tmp, bytes);
-#if TIME
-		struct timeval tp_e;
-		if (gettimeofday(&tp_e, 0) != 0)
-		   cerr << "could not read time" << endl;
 
-		cerr << "time to start thread: " << time_diff_to_hundredths(&tp_e, &tp_s) << endl;
-#endif
+		struct timeval tp_e;
+		if (time) {
+		  if (gettimeofday(&tp_e, 0) != 0)
+		    cerr << "could not read time" << endl;
+	      
+		  cerr << "time to fork thread: " << time_diff_to_hundredths(&tp_e, &tp_s) << endl;
+		}
             }
             else {
-	        MSG(cerr << "writing sequentially..." << endl);
-#if TIME
+	        if (messages) cerr << "writing sequentially..." << endl;
+
 		struct timeval tp_s;
-		if (gettimeofday(&tp_s, 0) != 0)
-		    cerr << "could not read time" << endl;
-#endif
+		if (time && gettimeofday(&tp_s, 0) != 0)
+		  cerr << "could not read time" << endl;
+
                 out.write(tmp, bytes);
                 delete tmp; tmp = 0;
-#if TIME
-		struct timeval tp_e;
-		if (gettimeofday(&tp_e, 0) != 0)
-		    cerr << "could not read time" << endl;
 
-		cerr << "time to write: " << time_diff_to_hundredths(&tp_e, &tp_s) << endl;
-#endif
+		struct timeval tp_e;
+		if (time) {
+		  if (gettimeofday(&tp_e, 0) != 0)
+		    cerr << "could not read time" << endl;
+	      
+		  cerr << "time to write: " << time_diff_to_hundredths(&tp_e, &tp_s) << endl;
+		}
+
             }
         }
     }
